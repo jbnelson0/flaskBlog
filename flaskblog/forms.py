dfabd56d287b0385345a04311bbf189f5804806f
Jamie Nelson <james.bnelson0@gmail.com>
2019-06-01 13:04:33 -0400

    Account edit

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
+from flask_login import current_user
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
@@ -27,3 +28,24 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            #if username is anything different from data in db then validate
+            user = User.query.filter_by(username=username.data).first()
+            #if user is anything other than none - throw error
+            if user:
+                raise ValidationError('That username is taken. Please choose a new username.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            #if email is anything different from data in db then validate
+            user = User.query.filter_by(email=email.data).first()
+            #if user is anything other than none - throw error
+            if user:
+                raise ValidationError('That email is taken. Please choose a new email.')
